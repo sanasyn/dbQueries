ef661ad8d623f2b5f01d099c49c00f2088829cc2
Marissa Pels <marissapels@Marissas-MacBook-Pro.local>
2018-02-12 20:50:05 -0600

    fix config file

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ module.exports = {
 	node_env : process.env.NODE_ENV || 'dev',
 	port : process.env.NODE_ENV || 5432,
 	client : process.env.CLIENT || 'postgres',
-	user : process.env.USER || 'aact',
+	user : process.env.DB_USER || 'aact',
 	host : process.env.HOST || 'aact-db.ctti-clinicaltrials.org',
 	database : process.env.DATABASE || 'aact',
 	password : process.env.PASSWORD || 'aact',
