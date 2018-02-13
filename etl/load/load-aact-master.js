a1cc3f665498404843939f552cc6e7090b75d0dc
Whitney Wong <whitneywong@Whitneys-MacBook-Pro.local>
2018-02-12 21:39:23 -0600

    Cleaned up comment

--- etl/load/load-aact-master.js
+++ etl/load/load-aact-master.js
@@ -1,9 +1,6 @@
 'use strict';
 
-// const connect_local = require('../db-keys').local;
-
 const loadAactMaster = (res, db) => {
-  // const db = require('knex')({client: 'pg', connection: connect_local})
   res.map((val) => {
     const insertQuery = "INSERT INTO public.aact_master VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)";
     db.raw(insertQuery, val)
