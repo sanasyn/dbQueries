eb9058a1127b5b86f049cf9b9b5c0cb662456cb8
Whitney Wong <whitneywong@Whitneys-MacBook-Pro.local>
2018-02-14 21:31:37 -0600

    Added code to truncate table before inserting new data.

--- etl/load/load-aact-master.js
+++ etl/load/load-aact-master.js
@@ -1,10 +1,13 @@
 'use strict';
 
 const loadAactMaster = (res, db) => {
-  res.map((val) => {
-    const insertQuery = "INSERT INTO public.aact_master VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)";
-    db.raw(insertQuery, val)
-      .catch((err) => console.log(err));
+  db.raw(`TRUNCATE aact_master`)
+  .then(() => {
+    res.map((val) => {
+      const insertQuery = "INSERT INTO public.aact_master VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)";
+      db.raw(insertQuery, val)
+        .catch((err) => console.log(err));
+    })
   })
 }
 
