6b8f066344d92cd7b5f44522c819aaa3243ade13
Whitney Wong <whitneywong@Whitneys-MacBook-Pro.local>
2018-02-16 02:00:23 -0600

    Changed studies ETL to contacts ETL. Cleaned up config.js. Added sql file for contact queries.

--- config/config.js
+++ config/config.js
@@ -1,17 +1,5 @@
 'use strict'
 
-// module.exports = {
-// 	node_env : process.env.NODE_ENV || 'dev',
-// 	port : process.env.NODE_ENV || 5432,
-// 	client : process.env.CLIENT || 'postgres',
-// 	user : process.env.DB_USER || 'aact',
-// 	host : process.env.HOST || 'aact-db.ctti-clinicaltrials.org',
-// 	database : process.env.DATABASE || 'aact',
-// 	password : process.env.PASSWORD || 'aact',
-// 	poolMin : process.env.poolMin || '0',
-// 	poolMax : process.env.poolMax || '100',
-// }
-
 module.exports = {
 	aact: {
 		node_env : process.env.NODE_ENV || 'dev',
---
+++ etl/transform/transform-contact-info.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const transformContacts = (res) => {
+  return res.map((val) => {
+    return [
+      val.nct_id,
+      val.condition,
+      val.central_contact_type,
+      val.central_contact_name,
+      val.central_contact_phone,
+      val.central_contact_email,
+      val.facility_contact_type,
+      val.facility_contact_name,
+      val.facility_contact_phone,
+      val.facility_contact_email
+    ]
+  })
+};
+
+module.exports = transformContacts;
