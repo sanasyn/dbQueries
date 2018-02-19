68ed09c9a403eefbdb8efcffa23e897876d15d21
Marissa Pels <marissapels@Marissas-MacBook-Pro.local>
2018-02-18 20:38:34 -0600

    revert config

--- config/config.js
+++ config/config.js
@@ -1,25 +1,31 @@
 'use strict'
 
+// module.exports = {
+// 	node_env : process.env.NODE_ENV || 'dev',
+// 	port : process.env.NODE_ENV || 5432,
+// 	client : process.env.CLIENT || 'postgres',
+// 	user : process.env.DB_USER || 'aact',
+// 	host : process.env.HOST || 'aact-db.ctti-clinicaltrials.org',
+// 	database : process.env.DATABASE || 'aact',
+// 	password : process.env.PASSWORD || 'aact',
+// 	poolMin : process.env.poolMin || '0',
+// 	poolMax : process.env.poolMax || '100',
+// }
+
 module.exports = {
-	node_env : 'dev',
-	aact:{	
-		port : 5432,
-		client : 'postgres',
-		user : 'aact',
-		host : 'aact-db.ctti-clinicaltrials.org',
-		database : 'aact',
-		password : 'aact',
-		poolMin : '0',
-		poolMax : '100',
+	aact: {
+		node_env : process.env.NODE_ENV || 'dev',
+		port : process.env.NODE_ENV || 5432,
+		client : process.env.CLIENT || 'postgres',
+		user : process.env.DB_USER || 'aact',
+		host : process.env.HOST || 'aact-db.ctti-clinicaltrials.org',
+		database : process.env.DATABASE || 'aact',
+		password : process.env.PASSWORD || 'aact',
+		poolMin : process.env.poolMin || '0',
+		poolMax : process.env.poolMax || '100',		
+		connectionString : process.env.CONNECTION_AACT || 'postgres://aact:aact@aact-db.ctti-clinicaltrials.org:5432/aact'
 	},
 	local: {
-		client : 'postgres',
-		user: 'marissapels',
-	    host: 'localhost',
-	    database: 'trials_local',
-	    password: process.env.PASSWORD || 'noPassword',
-	    port: 5432,
-	    searchPath: 'public'
+		connectionString : process.env.DATABASE_URL || 'postgres://whitneywong:test@localhost/testdb'	
 	}
-
 }
