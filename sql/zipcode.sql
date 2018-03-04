ed6f60f8bd8e034412f53d9c66b55b3d3480d838
Whitney Wong <whitneywong@cpe-172-100-3-238.twcny.res.rr.com>
2018-03-04 12:10:41 -0600

    Zipcode table

---
+++ sql/zipcode.sql
@@ -0,0 +1,10 @@
+create table zipcodes (
+    country varchar(15)
+    zipcode varchar(15),
+    city varchar(500),
+    state varchar(100),
+    latitude varchar(15),
+    longitude varchar(15),
+    classification_code varchar(5),
+    population varchar(50)
+)
