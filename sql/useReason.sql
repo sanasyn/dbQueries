8427c95ee5a0de4170623b04312658f713ed105d
Whitney Wong <whitneywong@Whitneys-MacBook-Pro.local>
2018-05-06 20:43:29 -0500

    collecting demographic data along with use reason to use the app. changed table so that it reflects this change

--- sql/useReason.sql
+++ sql/useReason.sql
@@ -1,12 +1,18 @@
 -- Create table sql --
 drop table use_reason;
+drop table user_demographics;
 
-create table use_reason (
+create table user_demographics (
+  timestamp timestamp,
+  age smallint,
+  sex varchar(15),
+  race varchar,
+  zip integer,
   family_history boolean,
   research_interest boolean,
   memory_complaints boolean,
-  other boolean,
-  other_reason varchar
+  other_reason_select boolean,
+  other_reason_text varchar
 );
 
-select * from use_reason;
+select * from user_demographics;
